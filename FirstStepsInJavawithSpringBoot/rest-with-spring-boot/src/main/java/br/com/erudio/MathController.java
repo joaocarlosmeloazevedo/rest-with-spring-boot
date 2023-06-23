73be1b4cb8db02599724c85b0d7fbe5f14670f4d
joaocarlosmeloazevedo <joaocarlos.mazevedo12@gmail.com>
2023-06-23 17:22:29 -0300

    Adicionado tratamento de excecao na classe MathController

--- FirstStepsInJavawithSpringBoot/rest-with-spring-boot/src/main/java/br/com/erudio/MathController.java
+++ FirstStepsInJavawithSpringBoot/rest-with-spring-boot/src/main/java/br/com/erudio/MathController.java
@@ -1,6 +1,6 @@
 package br.com.erudio;
 
-import java.util.concurrent.atomic.AtomicLong;
+//import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,29 +10,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController //Fornece o mapeamento entre o Path da Request e o método em questão.
 public class MathController {
 
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+//	private static final String template = "Hello, %s!";
+//	private final AtomicLong counter = new AtomicLong();
 	
 	@RequestMapping(value="/sum/{numberOne}/{numberTwo}", //Essa Annotation utiliza parametros obrigatórios na URL PathParameters. (Por este motivo o value)
 			method=RequestMethod.GET) //Especificando o tipo de requestMethod por boas práticas.
 			
 	public Double sum(
 			@PathVariable(value = "numberOne") String numberOne,//Definição das variavéis
-			@PathVariable(value = "numberTwo") String numberTwo //Definição das variavéis
-			) throws Exception{
+			@PathVariable(value = "numberTwo") String numberTwo) 
+			throws Exception{
 		
-		if (!isNumeric(numberOne) || isNumeric(numberTwo)) {
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
 			throw new Exception();
 		}
 		return convertToDouble(numberOne) + convertToDouble(numberTwo);
 	} 
 	
 	private Double convertToDouble(String strNumber) {
-		// TODO Auto-generated method stub
-		return null;
+		if (strNumber == null) return 0D;
+		String number = strNumber.replaceAll(",", "."); //Recebendo vírgula e retornando ponto
+		if (isNumeric(number)) return Double.parseDouble(number);
+		return 0D;
 	}
 
 	private boolean isNumeric(String strNumber) {
-		return false;
+		if (strNumber == null) return false;
+		String number = strNumber.replaceAll(",", "."); //Recebendo vírgula e retornando ponto
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+"); //Verificando se o número é negativo ou positivo, inteiro ou quebrado e se vai de 0 a 9.
 	}
 }
